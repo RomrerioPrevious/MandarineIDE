2a003aa1457e4064e075e241a18550b55e11b9e7
RomrerioPrevious <ggJohn12345@yandex.ru>
2023-11-09 23:26:39 +0300

    With veiw

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+from bin.view.view import Ui_MainWindow
+
+
 def main():
-    ...
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
 
 
 if __name__ == "main":
